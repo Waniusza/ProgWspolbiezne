f35b65ab71c15b6b7f9ea26a53b57f356856c54b
student <student@10.60.10.114>
2015-09-30 15:23:17 +0200

    init

---
+++ Lesson1/src/com/waniusza/lesson1/Ex1.java
@@ -0,0 +1,13 @@
+package com.waniusza.lesson1;
+
+public class Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
